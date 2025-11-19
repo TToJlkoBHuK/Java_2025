77e050844cb961c4f328de78e2d68912fa43768d
TToJlkoBHuK <28arhalex@gmail.com>
2025-11-19 19:07:24 +0300

    task_1

---
+++ консольный ввод - вывод/vvod.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class vvod {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        String Name = Scan.next();
+        System.out.printf("Hello, "+ Name);
+    }
+}
